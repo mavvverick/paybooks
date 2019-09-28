5f197f7c7c7807f890882f6d3ed4ac68e4fa9808
totality <paritosh@noisyox.com>
2019-09-28 15:05:48 +0530

    hotfix remove comment from rzp validate

--- services/booking.js
+++ services/booking.js
@@ -41,15 +41,15 @@ async function intiBooking (req, res, next) {
 }
 
 function commitBooking (req, res, next) {
-  // const validate = rzp.ValidateOrder(req.body.data)
+  const validate = rzp.ValidateOrder(req.body.data)
   const update = {
     paymentId: req.body.data.paymentId,
     status: 'DONE'
   }
 
-  // if (!validate) {
-  //   throw Error('Payment data can not be validated, contact support')
-  // }
+  if (!validate) {
+    throw Error('Payment data can not be validated, contact support')
+  }
 
   const bookId = parseInt(req.body.data.bookId)
 
